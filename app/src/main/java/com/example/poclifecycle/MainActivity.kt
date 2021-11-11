e2fe5a1eb501827dfa5c86d4055c1ee876b99906
Varas Estrada Giuseppe <gvarasex@bice.cl>
2021-11-10 23:47:42 -0300

    add mediaPlayer in Activity LifeCycle

--- app/src/main/java/com/example/poclifecycle/MainActivity.kt
+++ app/src/main/java/com/example/poclifecycle/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.poclifecycle
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
+    private var position: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,21 +18,29 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         Log.d("LifeCycle", "onStart")
+        mediaPlayer = MediaPlayer.create(this, R.raw.ai_2)
     }
 
     override fun onResume() {
         super.onResume()
+        mediaPlayer?.seekTo(position)
         Log.d("LifeCycle", "onResume")
+        mediaPlayer?.start()
     }
 
     override fun onPause() {
         super.onPause()
         Log.d("LifeCycle", "onPause")
+        mediaPlayer?.pause()
+        position = mediaPlayer!!.currentPosition
     }
 
     override fun onStop() {
         super.onStop()
         Log.d("LifeCycle", "onStop")
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 
     override fun onRestart() {
